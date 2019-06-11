554543276ef619590cfe2f4fa08a807a04001bf8
Andrew Allen <asallen67@gmail.com>
2019-06-11 13:16:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+
+	static capitalize(str) {
+		return str[0].toUpperCase() + str.slice(1)
+	}
+
+	// \w = [^0-9a-zA-Z_]
+	static sanitize(str) {
+		return str.replace(/[^\w '-]/g, '')
+	}
+
+	static titleize(str) {
+    const dont_change = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let title = [Formatter.capitalize(str.split(' ')[0])]
+    str.split(' ').slice(1).forEach(word => {
+    	dont_change.includes(word) ? title.push(word) : title.push(Formatter.capitalize(word))
+    })
+    return title.join(' ')
+	}
 }
